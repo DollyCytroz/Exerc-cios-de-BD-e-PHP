8cfbdcab35735152a4761cf5fd25eb9428d648b1
DollyCytroz <kauemcarreiro11@gmail.com>
2024-10-02 22:39:40 -0300

    Integracao_BD_com_PHP

--- Questões_Banco/cadastrar.php
+++ Questões_Banco/cadastrar.php
@@ -7,19 +7,23 @@
 </head>
 <body>
 
-    <center>
+<p> <h1> Insira os dados de cadastro </h1> </p> 
+
     <form action="valida_cadastro.php" method="POST">
-        <label for="marca">id:</label>
+        <label for="id">id:</label>
         <input type="text" name="id" require/>
+        <br>
+        <br>
         <label for="marca">marca:</label>
         <input type="text" name="marca" require/>
+        <br>
         <br>
         <label for="modelo">modelo:</label>
         <input type="text" name="modelo" require/>
         <br>
+        <br>
         <input type="submit" value="enviar"/>
     </form>
-    </center>
 
 </body>
 </html>
